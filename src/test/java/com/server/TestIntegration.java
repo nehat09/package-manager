e63ad0c39ec3a47dc5e40b5b65691cde3d670df3
Neha T <19446481+nehat09@users.noreply.github.com>
2017-10-17 19:44:09 -0400

    Improved integration test for sending more requests and waiting for completion of all clients

--- src/test/java/com/server/TestIntegration.java
+++ src/test/java/com/server/TestIntegration.java
@@ -4,8 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.junit.Test;
@@ -14,7 +14,7 @@ public class TestIntegration {
 
 	static final int MAX_THREADS = 100;
 	static final int TEST_PORT = 9092;
-	Executor e = Executors.newFixedThreadPool(MAX_THREADS+1);
+	Executor e = Executors.newFixedThreadPool(MAX_THREADS + 1);
 
 	/**
 	 * Load test for server. Start the server and send messages from multiple
@@ -33,15 +33,18 @@ public class TestIntegration {
 			}
 		}.start();
 
+		// create client requests
+		ArrayList<Thread> threads = new ArrayList<Thread>();
 		for (int i = 0; i < MAX_THREADS; i++) {
-			((ExecutorService) e).submit(new Runnable() {
-				public void run() {
-					Client client = new Client();
-					client.start();
-				}
-			});
+			Client client = new Client();
+			client.start();
+			threads.add(client);
 		}
 
+		// wait for all threads to complete
+		for (Thread thread : threads) {
+			thread.join();
+		}
 	}
 
 	/**
@@ -53,20 +56,46 @@ public class TestIntegration {
 			String hostName = "localhost";
 			int portNumber = TEST_PORT;
 
-			System.out.println("Client started..");
+			// System.out.println("Client started..");
 			try (Socket socket = new Socket(hostName, portNumber);
 					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-					BufferedReader in = new BufferedReader(
-							new InputStreamReader(socket.getInputStream()));) {
+					BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));) {
 
+				// send indexing requests
 				for (int i = 0; i < 500; i++) {
 					out.println("INDEX|aaa" + i + "|");
-					Thread.sleep(1000);
+					Thread.sleep(1);
+					in.readLine();
+				}
+
+				// send indexing requests with deps
+				for (int i = 0; i < 500; i++) {
+					out.println("INDEX|bbb" + i + "|" + generateRandomDeps());
+					Thread.sleep(1);
+					in.readLine();
+				}
+				
+				// send remove requests
+				for (int i = 0; i < 500; i++) {
+					out.println("REMOVE|bbb" + i + "|" + generateRandomDeps());
+					Thread.sleep(1);
+					in.readLine();
 				}
 
 			} catch (Exception e) {
 				throw new RuntimeException(e);
 			}
+		}
+
+		String generateRandomDeps() {
+			StringBuffer d = new StringBuffer();
+			int random = (int) Math.random() % 10;
+			int i;
+			for (i = 1; i < random; i++) {
+				d.append("aaa" + i + ",");
+			}
+			d.append("aaa" + i);
+			return d.toString();
 		}
 	}
 }
