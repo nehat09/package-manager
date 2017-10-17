403b65a1e417bf1202dccb8cccd21a0bf087c8bc
Neha T <19446481+nehat09@users.noreply.github.com>
2017-10-17 18:01:08 -0400

    Adding comments in Indexer

--- src/main/java/com/indexer/Indexer.java
+++ src/main/java/com/indexer/Indexer.java
@@ -2,9 +2,13 @@ package com.indexer;
 
 import java.util.List;
 
+/**
+ * Interface Indexer for handling all indexing operations,
+ * namely, index, remove and query.
+ */
 public interface Indexer {
 
-	 boolean index(String pkg, List<String> deps);
+	boolean index(String pkg, List<String> deps);
 
 	boolean remove(String name);
 
