294bcdbe91267aa818e07404a74e619941181a51
Neha T <19446481+nehat09@users.noreply.github.com>
2017-10-16 20:53:11 -0400

    Comments for PMMessageHandler

--- src/main/java/com/server/PMMessageHandler.java
+++ src/main/java/com/server/PMMessageHandler.java
@@ -13,6 +13,10 @@ public class PMMessageHandler {
 		this.manager = pm;
 	}
 
+	/**
+	 * Handler for messages that come into the PackageManager Server.
+	 * Returns output is format : OK, FAIL or ERROR.
+	 * */
 	public String processInput(String input) {
 		String output = ResponseCode.OK.toString();
 
@@ -31,6 +35,8 @@ public class PMMessageHandler {
 		return output;
 	}
 
+	/**
+	 * Process the command from input.  */
 	boolean processCommand(String input){
 		String command[] = input.split("\\|");
 		ArrayList<String> deps = null;
@@ -42,6 +48,8 @@ public class PMMessageHandler {
 		return status;
 	}
 	
+	/**
+	 * Get the dependencies as array list. */
 	ArrayList<String> getDependenciesFromInput(String depString){
 		ArrayList<String> deps = null;
 		deps = new ArrayList<String>();
@@ -51,6 +59,9 @@ public class PMMessageHandler {
 		return deps;
 	}
 	
+	/**
+	 * Perform the corresponding indexing operation based
+	 * on command, that is, INDEX, REMOVE or QUERY. */
 	boolean performIndexing(String command, String pkg, ArrayList<String> deps){
 		boolean status = false;
 		switch (command) {
@@ -67,6 +78,9 @@ public class PMMessageHandler {
 		return status;
 	}
 	
+	/**
+	 * Check for input validity based on command, package name 
+	 * and dependencies. */
 	boolean isValidInput(String in) {
 		String parts[] = in.split("\\|");
 
@@ -85,6 +99,9 @@ public class PMMessageHandler {
 		return true;
 	}
 
+	/**
+	 * Check for validity of dependencies, that is, they should be
+	 * correctly comma-separated and be valid package names. */
 	boolean isValidDependenciesList(String deps) {
 		if (deps.charAt(deps.length() - 1) == ',') {
 			return false;
@@ -99,12 +116,19 @@ public class PMMessageHandler {
 		return true;
 	}
 
+	/**
+	 * Check for valid package name, like apt or Homebrew packages. 
+	 * NOTE: due to varying rules of where a + is allowed in package name,
+	 * two exceptions from the Test Harness were explicitly checked.
+	 * Namely, "gtk+3" and "dvd+rw-tools". */
 	boolean isValidPackageName(String name) {
 		boolean isValid = name.matches("[-_\\p{Alnum}]+[+]*") || name.equals("gtk+3")
 				|| name.equals("dvd+rw-tools");
 		return isValid;
 	}
 
+	/**
+	 * Checks if command is one of INDEX, QUERY or REMOVE. */
 	boolean isValidCommand(String command) {
 		String commandList[] = { "INDEX", "QUERY", "REMOVE" };
 		return Arrays.asList(commandList).contains(command);
